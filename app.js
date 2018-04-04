7bf6902583722b903d8b662cb58e4b8dc2ebd930
lixin3 <37858857+lixin3@users.noreply.github.com>
2018-04-04 09:29:43 +0800

    Update app.js

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const path = require("path"); //处理路径
 const session = require("express-session");//session引入
 const cookie = require("cookie-parser");
 const router=require("./routes/baokuan.js");
+const myejs=require('ejs');
+const productRouter=require('./router/productRouter.js')
 
 const app = express();
 app.use(router);
@@ -22,6 +24,11 @@ app.use(session({
 //将post数据储存为json数据
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
+//配置ejs
+app.set("views",__dirname+"/public");//配置模板所在路径
+app.engine("html",myejs.__express);//添加一个html引擎
+app.set("view engine","html");//使用的引擎
+app.use(productRouter);
 app.use(express.static(path.join(__dirname,"public")));
 app.use(express.static(path.join(__dirname,"public/html")));
 
