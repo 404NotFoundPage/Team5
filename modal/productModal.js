d104a1820a50de1fea2ad9fbe25074b67dcc541e
lixin3 <37858857+lixin3@users.noreply.github.com>
2018-04-08 09:59:47 +0800

    Update productModal.js

--- modal/productModal.js
+++ modal/productModal.js
@@ -1,13 +1,26 @@
-/**
- * Created by yang on 2018/3/12.
- */
+/* Created by lixin on 2018/3/12.*/
 let sqlpool = require('./sqlcool.js');
 const mysql=require('mysql');
 const db = sqlpool.sqlpool();
 module.exports={
     productDetail:function(pro_id,callback){
-        let sql="select * from t_productimg where pro_id=?";
+        let sql="select * from t_productinfo,t_producttext,t_productimg where t_productinfo.pro_id=t_producttext.pro_id and " +
+            " t_productinfo.pro_id=t_productimg.pro_id and t_producttext.pro_id=?"
         db.connect(sql,pro_id,callback);
-        console.log(pro_id)
+    },
+    comment:function(pro_id,pagesize,currentpage,callback){
+        let start=(currentpage-1)*pagesize;
+        let end=parseInt(pagesize);
+        let arr=[pro_id,start,end];
+        let sql="select * from t_comment,t_user where t_user.user_id=t_comment.user_id and t_comment.pro_id=? limit ?,?"
+        db.connect(sql,arr,callback);
+    },
+    gettotalcount:function(pro_id,callback){
+        let sql="select * from t_comment,t_user where t_user.user_id=t_comment.user_id and t_comment.pro_id=?";
+        db.connect(sql,pro_id,callback);
+    },
+    reply:function(pro_id,callback){
+        let sql="select * from t_comment where com_reply_state=1 and pro_id=?";
+        db.connect(sql,pro_id,callback);
     }
 };
