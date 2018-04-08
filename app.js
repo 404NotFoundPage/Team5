4652b4e8a22cabdb6edc9aaebf29feb742d4a1e8
lixin3 <37858857+lixin3@users.noreply.github.com>
2018-04-08 09:51:49 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 /**
  * Created by Administrator on 2018/3/30 0030.
  */
@@ -8,38 +8,9 @@ const path = require("path"); //处理路径
 const session = require("express-session");//session引入
 const cookie = require("cookie-parser");
 const router=require("./routes/baokuanrouter.js");
-
-const app = express();
-app.use(router);
-app.use(cookie());
-app.use(session({
-    name:"cms",
-    secret:"123",
-    cookie:{maxAge:300000},
-    resave:true,
-    rolling:true,
-    saveUninitialized:true
-}));
-//将post数据储存为json数据
-app.use(bodyParser.urlencoded({extended:false}));
-app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname,"/")));
-app.use(express.static(path.join(__dirname,"public/html")));
-
-//监听端口
-app.listen(1111,()=>{console.log("服务器1111启动")});
-=======
-/**
- * Created by Administrator on 2018/3/30 0030.
- */
-const express = require("express");
-const bodyParser = require("body-parser");  //处理POST数据
-const path = require("path"); //处理路径
-const session = require("express-session");//session引入
-const cookie = require("cookie-parser");
-const router=require("./routes/baokuan.js");
+const productRouter=require('./routes/productRouter.js')
 const myejs=require('ejs');
-const productRouter=require('./router/productRouter.js')
+
 
 const app = express();
 app.use(router);
@@ -60,9 +31,8 @@ app.set("views",__dirname+"/public");//配置模板所在路径
 app.engine("html",myejs.__express);//添加一个html引擎
 app.set("view engine","html");//使用的引擎
 app.use(productRouter);
-app.use(express.static(path.join(__dirname,"public")));
+app.use(express.static(path.join(__dirname,"/")));
 app.use(express.static(path.join(__dirname,"public/html")));
 
 //监听端口
 app.listen(1111,()=>{console.log("服务器1111启动")});
->>>>>>> 7bf6902583722b903d8b662cb58e4b8dc2ebd930
